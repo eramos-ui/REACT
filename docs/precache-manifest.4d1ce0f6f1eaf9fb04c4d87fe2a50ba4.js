14730ce1fbf7cd31940f3cfc52369ba493b72f97
eramos-ui <eramosarellano@gmail.com>
2020-08-10 13:13:20 -0400

    docs agregado

---
+++ docs/precache-manifest.4d1ce0f6f1eaf9fb04c4d87fe2a50ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b3aec22277d6dac48ef6998b16d68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cad724d6dd2b8cd2139",
+    "url": "/static/css/main.e44c5577.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cad724d6dd2b8cd2139",
+    "url": "/static/js/main.e7104821.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
